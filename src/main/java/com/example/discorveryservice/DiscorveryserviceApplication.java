3ffa4f2ebff68b1388d96c219ab848d528b711a3
lby132 <quddbscjswow@naver.com>
2022-09-19 15:08:34 +0900

    eureka server

---
+++ src/main/java/com/example/discorveryservice/DiscorveryserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.discorveryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscorveryserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscorveryserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/discorveryservice/DiscorveryserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.discorveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscorveryserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
